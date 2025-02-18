3da04d0a0078d31afc4fa37a25dcda4384423d4d
KABANGA David <Davkabanga96@gmail.com>
2025-02-19 00:15:40 +0200

    Implementing functionnalities

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -23,9 +23,9 @@ const Navbar = () => {
     }, [])
 
     return (
-        <nav className="flex items-center justify-around w-full pt-4 px-10 bg-white border-b border-gray-100 shadow-md">
+        <nav className="flex items-center justify-between w-full pt-4 px-10 bg-white border-b border-gray-100 shadow-md">
 
-            <Link href="" className=" font-extrabold mb-4 sm:mb-2">
+            <Link href="" className=" font-extrabold mb-4">
                 <h1 className="flex items-center justify-center gap-1 text-2xl">N Y U M B<House className="text-green-600 font-extrabold" />N I </h1>
             </Link>
             {session?.user ? (
@@ -92,6 +92,7 @@ const Navbar = () => {
 
 
             {/* Desktop Navigation */}
+            
 
 
 
