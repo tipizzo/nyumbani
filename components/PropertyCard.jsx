5a62faf6063487e9d111d755d889105f52fb1175
KABANGA David <Davkabanga96@gmail.com>
2025-02-19 16:32:10 +0200

    Making the card property dynamic

--- components/PropertyCard.jsx
+++ components/PropertyCard.jsx
@@ -2,135 +2,40 @@
 
 import { Star } from "lucide-react"
 import Image from "next/image"
+import properties from "@/utils/data";
 
 const PropertyCard = () => {
     return (
         <>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-16 w-max mx-auto py-14 px-4">
-
-                <div className="flex flex-col gap-1 max-w-[350px] cursor-pointer rounded-xl shadow-xl hover:bg-gray-100 transition-all duration-300 mb-8 pb-5">
-                    <Image
-                        src="/assets/images/apart_1.jpg"
-                        width={350}
-                        height={300}
-                        alt=""
-                        className="rounded-xl"
-                    />
-                    <span className="absolute w-max text-[10px] bg-orange-600 text-white m-3 p-1 rounded-xl">Pending</span>
-                    <div className="flex flex-col gap-2 mt-5 px-2">
-                        <div className="flex items-center justify-between">
-                            <p className="text-[15px] font-bold">Himbi - Goma/DRC</p>
-                            <p className="flex gap-1 text-[15px] font-bold items-center"><Star className="" /></p>
+                {properties.map(property => (
+                    <div key={property.id} className="flex flex-col gap-1 max-w-[350px] cursor-pointer rounded-xl shadow-xl hover:bg-gray-100 transition-all duration-300 mb-8 pb-5">
+                        <Image
+                            src={property.image}
+                            width={350}
+                            height={300}
+                            alt=""
+                            className="rounded-xl"
+                        />
+                        <span className={`absolute w-max text-[10px] 
+                            ${property.availability === "available" ? "bg-green-600" 
+                            : property.availability === "Unavailable" ? "bg-red-600"
+                            : property.availability === "Pending" ? "bg-orange-600" 
+                            : "bg-gray-600"}
+                             text-white m-3 p-1 rounded-xl`}>{property.availability}</span>
+                        <div className="flex flex-col gap-2 mt-5 px-2">
+                            <div className="flex items-center justify-between">
+                                <p className="text-[15px] font-bold">{property.neighborhood}, {property.city} - {property.country}</p>
+                                <p className="flex gap-1 text-[15px] font-bold items-center"><Star className="" /></p>
+                            </div>
+                            <p className="text-sm font-light">{property.description}</p>
+                            <p className="text-sm font-semibold">Bedroom: {property.bedroom}</p>
+                            <p className="text-[15px] font-bold">{property.monthly_price} $ par mois</p>
+                            <p className="text-[15px] font-bold">{property.daily_price} $ par jour</p>
                         </div>
-                        <p className="text-sm font-light">Cet appartement situé au bord du lac Kivu vous offre une vue unique sur le lac...</p>
-                        <p className="text-sm font-semibold">1 livingroom, 3 bedrooms, 2 bathrooms, 1 kitchen</p>
-                        <p className="text-[15px] font-bold">500 $ par mois</p>
-                        <p className="text-[15px] font-bold">50 $ par jour</p>
                     </div>
-                </div>
-                <div className="flex flex-col gap-1 max-w-[350px] cursor-pointer rounded-xl shadow-xl hover:bg-gray-100 transition-all duration-300 mb-8 pb-5">
-                    <Image
-                        src="/assets/images/apart_3.jpg"
-                        width={350}
-                        height={300}
-                        alt=""
-                        className="rounded-xl"
-                    />
-                    <span className="absolute w-max text-[10px] bg-green-600 text-white m-3 p-1 rounded-lg">Available</span>
-                    <div className="flex flex-col gap-2 mt-5 px-2">
-                        <div className="flex items-center justify-between">
-                            <p className="text-[15px] font-bold">Les Volcans - Goma/DRC</p>
-                            <p className="flex gap-1 text-[15px] font-bold items-center"><Star className="" /></p>
-                        </div>
-                        <p className="text-sm font-light">Cet appartement situé au bord du lac Kivu vous offre une vue unique sur le lac...</p>
-                        <p className="text-sm font-semibold">1 livingroom, 3 bedrooms, 2 bathrooms, 1 kitchen</p>
-                        <p className="text-[15px] font-bold">500 $ par mois</p>
-                        <p className="text-[15px] font-bold">50 $ par jour</p>
-                    </div>
-                </div>
-                <div className="flex flex-col gap-1 max-w-[350px] cursor-pointer rounded-xl shadow-xl hover:bg-gray-100 transition-all duration-300 mb-8 pb-5">
-                    <Image
-                        src="/assets/images/apart_3.jpg"
-                        width={350}
-                        height={300}
-                        alt=""
-                        className="rounded-xl"
-                    />
-                    <span className="absolute w-max text-[10px] bg-red-600 text-white m-3 p-1 rounded-lg">Not available</span>
-                    <div className="flex flex-col gap-2 mt-5 px-2">
-                        <div className="flex items-center justify-between">
-                            <p className="text-[15px] font-bold">Kyeshero - Goma/DRC</p>
-                            <p className="flex gap-1 text-[15px] font-bold items-center"><Star className="" /></p>
-                        </div>
-                        <p className="text-sm font-light">Cet appartement situé au bord du lac Kivu vous offre une vue unique sur le lac...</p>
-                        <p className="text-sm font-semibold">1 livingroom, 3 bedrooms, 2 bathrooms, 1 kitchen</p>
-                        <p className="text-[15px] font-bold">500 $ par mois</p>
-                        <p className="text-[15px] font-bold">50 $ par jour</p>
-                    </div>
-                </div>
-                <div className="flex flex-col gap-1 max-w-[350px] cursor-pointer rounded-xl shadow-xl hover:bg-gray-100 transition-all duration-300 mb-8 pb-5 ">
-                    <Image
-                        src="/assets/images/apart_1.jpg"
-                        width={350}
-                        height={300}
-                        alt=""
-                        className="rounded-xl"
-                    />
-                    <span className="absolute w-max text-[10px] bg-green-600 text-white m-3 p-1 rounded-lg">Available</span>
-                    <div className="flex flex-col gap-2 mt-5 px-2">
-                        <div className="flex items-center justify-between">
-                            <p className="text-[15px] font-bold">Les Volcans - Goma/DRC</p>
-                            <p className="flex gap-1 text-[15px] font-bold items-center"><Star className="" /></p>
-                        </div>
-                        <p className="text-sm font-light">Cet appartement situé au bord du lac Kivu vous offre une vue unique sur le lac...</p>
-                        <p className="text-sm font-semibold">1 livingroom, 3 bedrooms, 2 bathrooms, 1 kitchen</p>
-                        <p className="text-[15px] font-bold">500 $ par mois</p>
-                        <p className="text-[15px] font-bold">50 $ par jour</p>
-                    </div>
-                </div>
-                <div className="flex flex-col gap-1 max-w-[350px] cursor-pointer rounded-xl shadow-xl hover:bg-gray-100 transition-all duration-300 mb-8 pb-5 ">
-                    <Image
-                        src="/assets/images/apart_1.jpg"
-                        width={350}
-                        height={300}
-                        alt=""
-                        className="rounded-xl"
-                    />
-                    <span className="absolute w-max text-[10px] bg-green-600 text-white m-3 p-1 rounded-lg">Available</span>
-                    <div className="flex flex-col gap-2 mt-5 px-2">
-                        <div className="flex items-center justify-between">
-                            <p className="text-[15px] font-bold">Les Volcans - Goma/DRC</p>
-                            <p className="flex gap-1 text-[15px] font-bold items-center"><Star className="" /></p>
-                        </div>
-                        <p className="text-sm font-light">Cet appartement situé au bord du lac Kivu vous offre une vue unique sur le lac...</p>
-                        <p className="text-sm font-semibold">1 livingroom, 3 bedrooms, 2 bathrooms, 1 kitchen</p>
-                        <p className="text-[15px] font-bold">500 $ par mois</p>
-                        <p className="text-[15px] font-bold">50 $ par jour</p>
-                    </div>
-                </div>
-                <div className="flex flex-col gap-1 max-w-[350px] cursor-pointer rounded-xl shadow-xl hover:bg-gray-100 transition-all duration-300 mb-8 pb-5 ">
-                    <Image
-                        src="/assets/images/apart_1.jpg"
-                        width={350}
-                        height={300}
-                        alt=""
-                        className="rounded-xl"
-                    />
-                    <span className="absolute w-max text-[10px] bg-green-600 text-white m-3 p-1 rounded-lg">Available</span>
-                    <div className="flex flex-col gap-2 mt-5 px-2">
-                        <div className="flex items-center justify-between">
-                            <p className="text-[15px] font-bold">Les Volcans - Goma/DRC</p>
-                            <p className="flex gap-1 text-[15px] font-bold items-center"><Star className="" /></p>
-                        </div>
-                        <p className="text-sm font-light">Cet appartement situé au bord du lac Kivu vous offre une vue unique sur le lac...</p>
-                        <p className="text-sm font-semibold">1 livingroom, 3 bedrooms, 2 bathrooms, 1 kitchen</p>
-                        <p className="text-[15px] font-bold">500 $ par mois</p>
-                        <p className="text-[15px] font-bold">50 $ par jour</p>
-                    </div>
-                </div>
+                ))}
             </div>
-
-
         </>
 
     )
---
+++ utils/data.js
@@ -0,0 +1,49 @@
+const properties = [
+    {
+        id: 1,
+        title: "property_1",
+        neighborhood: "Himbi",
+        city: "Goma",
+        country: "DRC",
+        description: "Cet appartement situé au bord du lac Kivu vous offre une vue unique sur le lac...",
+        livingroom: 1,
+        bedroom: 3,
+        bathroom: 2,
+        monthly_price: 500,
+        daily_price: 50,
+        availability: "available",
+        image: "/assets/images/apart_1.jpg"
+    },
+    {
+        id: 2,
+        title: "property_2",
+        neighborhood: "Kyeshero",
+        city: "Goma",
+        country: "DRC",
+        description: "Cet appartement situé au bord du lac Kivu vous offre une vue unique sur le lac...",
+        livingroom: 1,
+        bedroom: 3,
+        bathroom: 2,
+        monthly_price: 300,
+        daily_price: 40,
+        availability: "Unavailable",
+        image: "/assets/images/apart_2.jpg"
+    },
+    {
+        id: 3,
+        title: "property_2",
+        neighborhood: "Nguba",
+        city: "Bukavu",
+        country: "DRC",
+        description: "Cet appartement situé au bord du lac Kivu vous offre une vue unique sur le lac...",
+        livingroom: 1,
+        bedroom: 5,
+        bathroom: 2,
+        monthly_price: 600,
+        daily_price: 45,
+        availability: "Pending",
+        image: "/assets/images/apart_3.jpg"
+    },
+]
+
+export default properties;
