cfdf8a8424fcb5dd91f79276d5e2526f5c2d08cf
KABANGA David <Davkabanga96@gmail.com>
2025-02-20 11:34:50 +0200

    Design the mobile view of the home page

--- components/About.jsx
+++ components/About.jsx
@@ -6,9 +6,9 @@ import { ArrowBigRight, Bot, ChartArea, ListCheck, MessageCircle } from "lucide-
 const About = () => {
     return (
         <section className="flex flex-col items-center justify-center mt-16 w-full">
-            <h1 className="text-2xl font-semibold">Making Property Transactions Seamless & Hassle-Free</h1>
-            <div className="flex mt-10 justify-between w-full max-w-5xl gap-10">
-                <div className="flex flex-col items-center text-center w-1/3 p-5 border rounded-lg shadow-md gap-6">
+            <h1 className="text-xl sm:text-2xl font-semibold text-center">Making Property Transactions Seamless & Hassle-Free</h1>
+            <div className="flex flex-col sm:flex-row sm:justify-between mt-10 items-center w-full max-w-5xl gap-6">
+                <div className="flex flex-col items-center text-center w-2/3 sm:w-1/3 p-5 border rounded-lg shadow-md gap-6">
                     <ListCheck className="text-green-700 w-10 h-10" />
                     <h3 className="text-gray-800 font-bold">List Your Property</h3>
                     <p className="text-xs">Sell or rent your propery in just a few clicks</p>
@@ -16,7 +16,7 @@ const About = () => {
                         <Link href="" className="font-bold text-sm text-green-800 hover:text-green-500 transition-all duration-500">List Now...</Link>
                     </button>
                 </div>
-                <div className="flex flex-col items-center text-center w-1/3 p-5 border rounded-lg shadow-md gap-6">
+                <div className="flex flex-col items-center text-center w-2/3 sm:w-1/3 p-5 border rounded-lg shadow-md gap-6">
                     <MessageCircle className="text-green-700 w-10 h-10" />
                     <h3 className="text-gray-800 font-bold">Real-Time Chat</h3>
                     <p className="text-xs">Instantly communicate with property owners and agents.</p>
@@ -24,7 +24,7 @@ const About = () => {
                         <Link href="" className="font-bold text-sm text-green-800 hover:text-green-500 transition-all duration-500">Chat Now...</Link>
                     </button>
                 </div>
-                <div className="flex flex-col items-center text-center w-1/3 p-5 border rounded-lg shadow-md gap-6">
+                <div className="flex flex-col items-center text-center w-2/3 sm:w-1/3 p-5 border rounded-lg shadow-md gap-6">
                     <Bot className="text-green-700 w-10 h-10" />
                     <h3 className="text-gray-800 font-bold">24/7 Support</h3>
                     <p className="text-xs">Get assistance in securing home loans and mortgages.</p>
