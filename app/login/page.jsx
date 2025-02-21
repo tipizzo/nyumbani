31e03f2d550bfe4280ac6080dbdc45fff5e5da29
KABANGA David <Davkabanga96@gmail.com>
2025-02-21 12:25:03 +0200

    Finalizing the login design

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,8 +1,23 @@
+'use client'
+
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 
 const Login = () => {
+
+    const [state, setState] = useState('Login')
+
+    const [fullName, setFullName] = useState('')
+    const [phone, setPhone] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const [location, setLocation] = useState('')
+    const [userRole, setUserRole] = useState('Tenant')
+
+
     return (
         <div className="flex w-screen h-screen">
             <div className="relative w-1/2 h-full">
@@ -24,59 +39,74 @@ const Login = () => {
             </div>
             <div className="flex flex-col items-center justify-center w-1/2 gap-5 bg-gray-100">
                 <div className="flex flex-col gap-2 border px-6 py-3 bg-white rounded-xl shadow-md">
-                    <h2 className="font-bold text-2xl">Create an account</h2>
-                    <p className="text-sm text-slate-500">Please, Sign Up to explore more features</p>
+                    <h2 className="font-bold text-2xl">{state === "Sign Up" ? "Create an account" : "Login"}</h2>
+                    <p className="text-sm text-slate-500">Please, {state === "Sign Up" ? "Sign Up" : "Login"} to explore more features</p>
                     <form className="flex flex-col gap-2">
-                        <div className="flex flex-col gap-1">
-                            <label className="text-sm font-semibold">Full Name</label>
-                            <input type="text" placeholder="John Doe" className="p-2 text-sm w-[450px] border-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 transition-all duration-300" />
-                        </div>
-                        <div className="flex flex-col gap-1">
-                            <label className="text-sm font-semibold">Phone Number</label>
-                            <input type="text" placeholder="+243 974377888" className="p-2 text-sm w-[450px] border-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 transition-all duration-300" />
-                        </div>
+                        {state === "Sign Up"
+                            && <div className="flex flex-col gap-1">
+                                <label className="text-sm font-semibold">Full Name</label>
+                                <input type="text" placeholder="John Doe" className="p-2 text-sm w-[450px] border-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 transition-all duration-300" onChange={(e) => setFullName(e.target.fullName)} value={fullName} required />
+                            </div>
+                        }
+                        {state === "Sign Up"
+                            && <div className="flex flex-col gap-1">
+                                <label className="text-sm font-semibold">Phone Number</label>
+                                <input type="text" placeholder="+243 974377888" className="p-2 text-sm w-[450px] border-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 transition-all duration-300" onChange={(e) => setPhone(e.target.phone)} value={phone} required />
+                            </div>
+                        }
                         <div className="flex flex-col gap-1">
                             <label className="text-sm font-semibold">Email</label>
-                            <input type="email" placeholder="example@gmail.com" className="p-2 text-sm w-[450px] border-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 transition-all duration-300" />
+                            <input type="email" placeholder="example@gmail.com" className="p-2 text-sm w-[450px] border-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 transition-all duration-300" onChange={(e) => setEmail(e.target.email)} value={email} required />
                         </div>
                         <div className="flex flex-col gap-1">
                             <label className="text-sm font-semibold">Password</label>
-                            <input type="password" className="p-2 text-sm w-[450px] border-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 transition-all duration-300" />
+                            <input type="password" className="p-2 text-sm w-[450px] border-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 transition-all duration-300" onChange={(e) => setPassword(e.target.password)} value={password} required />
                         </div>
-                        <div className="flex flex-col gap-1">
-                            <label className="text-sm font-semibold">Confirm your Password</label>
-                            <input type="password" className="p-2 text-sm w-[450px] border-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 transition-all duration-300" />
-                        </div>
-                        <div className="flex flex-col gap-1">
-                            <label className="text-sm font-semibold">Location</label>
-                            <input type="text" placeholder="Himbi, Goma/DRC" className="p-2 text-sm w-[450px] border-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 transition-all duration-300" />
-                        </div>
-                        <div className="flex flex-col gap-1">
-                            <label className="text-sm font-semibold">User Role</label>
-                            <select className="p-2 text-sm w-[450px] border-2 rounded-lg">
-                                <option value="tenant">Tenant</option>
-                                <option value="tenant">Landlord</option>
-                                <option value="tenant">Agent</option>
-                            </select>
-                        </div>
+                        {state === "Sign Up"
+                            && <div className="flex flex-col gap-1">
+                                <label className="text-sm font-semibold">Confirm your Password</label>
+                                <input type="password" className="p-2 text-sm w-[450px] border-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 transition-all duration-300" onChange={(e) => setConfirmPassword(e.target.confirmPassword)} value={confirmPassword} required />
+                            </div>
+                        }
+                        {state === "Sign Up"
+                            && <div className="flex flex-col gap-1">
+                                <label className="text-sm font-semibold">Location</label>
+                                <input type="text" placeholder="Himbi, Goma/DRC" className="p-2 text-sm w-[450px] border-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 transition-all duration-300" onChange={(e) => setLocation(e.target.location)} value={location} required />
+                            </div>
+                        }
+                        {state === "Sign Up"
+                            && <div className="flex flex-col gap-1">
+                                <label className="text-sm font-semibold">User Role</label>
+                                <select className="p-2 text-sm w-[450px] border-2 rounded-lg" onChange={(e) => setUserRole(e.target.value)} value={userRole}>
+                                    <option value="tenant">Tenant</option>
+                                    <option value="tenant">Landlord</option>
+                                    <option value="tenant">Agent</option>
+                                </select>
+                            </div>
+                        }
+
                         <button className="font-bold border-1 border-green-600 p-2 mt-2 rounded-lg bg-gradient-to-r from-green-500 to-green-600 text-slate-50 text-sm cursor-pointer">
-                            Sign Up
+                            {state === "Sign Up" ? "Create Account" : "Login"}
                         </button>
                         <div className="flex justify-between gap-2">
-                            <button className="border-none bg-white p-1 w-[220px] rounded-lg text-sm hover:bg-gray-200 transition-all duration-300">
+                            <button className="bg-white p-1 w-[220px] border border-black rounded-lg text-sm hover:bg-gray-200 transition-all duration-300">
                                 <Link href="" className="flex gap-3 items-center text-center">
                                     <img src="/assets/images/google.png" className="w-8 h-8" />
                                     <p className="font-semibold">Connect with Google</p>
                                 </Link>
                             </button>
-                            <button className="border-none bg-white p-1 w-[220px] rounded-lg text-sm hover:bg-gray-200 transition-all duration-300">
+                            <button className="bg-white p-1 w-[220px] border border-black rounded-lg text-sm hover:bg-gray-200 transition-all duration-300">
                                 <Link href="" className="flex gap-3 items-center text-center">
                                     <img src="/assets/images/facebook.png" className="w-8 h-8" />
                                     <p className="font-semibold">Connect with Facebook</p>
                                 </Link>
                             </button>
                         </div>
-                        <p className="text-center text-sm mt-2">Already have an account ? <Link href="" className="text-blue-700 font-bold cursor-pointer">Login</Link></p>
+                        {
+                            state === "Sign Up"
+                            ? <p className="text-center text-sm mt-2">Already have an account ?<Link href="" className="text-blue-700 font-bold cursor-pointer" onClick={() => setState("Login")}>Login</Link></p>
+                            : <p className="text-center text-sm mt-2">Create a new account ?<Link href="" className="text-blue-700 font-bold cursor-pointer" onClick={() => setState("Sign Up")}>Sign Up</Link></p>
+                        }
                     </form>
                 </div>
             </div>
